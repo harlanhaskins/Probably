da1c7d3c734cc3e25b8d7812f03f08bef060c0d5
Harlan Haskins <harlan@harlanhaskins.com>
2016-10-07 23:10:33 -0400

    Fixed tests for fixed factorial behavior.

--- Tests/ProbablyTests/ProbablyTests.swift
+++ Tests/ProbablyTests/ProbablyTests.swift
@@ -5,8 +5,8 @@ class ProbablyTests: XCTestCase {
     
     func testPoisson() {
         let p = Poisson(mu: 2)
-        XCTAssertEqualWithAccuracy(p.probability(of: 2), 0.541, accuracy: 0.01)
-        XCTAssertEqualWithAccuracy(p.distribution(.lessOrEqual(5)), 2.03, accuracy: 0.01)
+        XCTAssertEqualWithAccuracy(p.probability(of: 2), 0.271, accuracy: 0.01)
+        XCTAssertEqualWithAccuracy(p.distribution(.lessOrEqual(5)), 0.983, accuracy: 0.01)
         XCTAssertEqualWithAccuracy(p.variance(), 2.0, accuracy: 0.01)
         XCTAssertEqualWithAccuracy(p.standardDeviation(), 1.414, accuracy: 0.01)
     }
@@ -32,6 +32,8 @@ class ProbablyTests: XCTestCase {
     static var allTests : [(String, (ProbablyTests) -> () throws -> Void)] {
         return [
             ("testPoisson", testPoisson),
+            ("testContinuous", testContinuous),
+            ("testHypergeometric", testHypergeometric)
         ]
     }
 }
